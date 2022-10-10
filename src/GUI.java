c4369ff7986ca23daf843df71d1c839244a4e86c
Philip <clashofclubs@gmail.com>
2022-10-10 20:29:54 +0200

    Started GUI

--- src/GUI.java
+++ src/GUI.java
@@ -2,7 +2,6 @@
 import java.awt.*;
 
 
-
 /**
  * Class that handles the creation and updating of the GUI using the {@code Java Swing} library.
  *
@@ -19,9 +18,9 @@ public class GUI {
         this.gameState = gameState;
 
         if (maze.length <= maze[0].length) { // if rows < cols, use rows to determine tile length and a vice versa
-            tileLength = 100 / (double) maze.length;
+            tileLength = 100 / maze.length;
         } else {
-            tileLength = 100 / (double) maze[0].length;
+            tileLength = 100 / maze[0].length;
         }
 
 
@@ -35,26 +34,29 @@ public class GUI {
         StdDraw.setYscale(-100, 100);
 
         StdDraw.setPenColor(StdDraw.BLACK);
-        StdDraw.filledRectangle(1920 / 2, 1080 / 2 + 100, 300, 300); // this will be the area that
+        StdDraw.filledRectangle(0, 0, 100, 100); // this will be the area that
 
 
-        double xCord = -70.00;
-        double yCord =  70.00;
+        double xCord = -70.00 + tileLength/2;
+        double yCord = 70.00 - tileLength/2;
+        StdDraw.setPenColor(StdDraw.BLUE);
+        StdDraw.filledSquare(0.0,0.0,70.00);
+
         for (int iRows = 1; iRows < maze.length; iRows++) { // loop through the tile maze and create tiles on Canvas
-
-            for(int iCol = 1; iCol < maze[0].length;iCol++) {
+            xCord = -70.00 + tileLength/2;
+            for (int iCol = 1; iCol < maze[0].length; iCol++) {
                 StdDraw.setPenColor(Color.RED);
-                StdDraw.filledSquare(xCord,yCord,tileLength/2);
+                StdDraw.filledSquare(xCord, yCord, tileLength / 2);
                 StdDraw.setPenColor(Color.BLACK);
-                StdDraw.square(xCord,yCord,tileLength/2);
-            // create tile in here
+                StdDraw.square(xCord, yCord, tileLength / 2);
+                // create tile in here
                 xCord = xCord + tileLength;
             }
             // when tile is done
-                yCord = yCord + tileLength;
+            yCord = yCord - tileLength;
 
         }
-
+        StdDraw.show();
 
 
     }
