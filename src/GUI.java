eb02e055497bcc8d59814549be6f61006d8fc13a
Philip de Bruyn <clashofclubs@gmail.com>
2022-10-10 20:01:42 +0200

    Attempted GUI things

--- src/GUI.java
+++ src/GUI.java
@@ -1,8 +1,5 @@
-import javax.swing.*;
-import javax.swing.text.JTextComponent;
+
 import java.awt.*;
-import java.awt.event.KeyEvent;
-import java.awt.event.KeyListener;
 
 
 
@@ -11,33 +8,60 @@ import java.awt.event.KeyListener;
  *
  * @author Philip de Bruyn
  */
-public class GUI  {
+public class GUI {
 
     private Tile[][] maze;
     private GameState gameState;
+    private double tileLength;
 
     public GUI(GameState gameState, Tile[][] maze) {
-    this.maze = maze;
-    this.gameState = gameState;
+        this.maze = maze;
+        this.gameState = gameState;
+
+        if (maze.length <= maze[0].length) { // if rows < cols, use rows to determine tile length and a vice versa
+            tileLength = 100 / (double) maze.length;
+        } else {
+            tileLength = 100 / (double) maze[0].length;
+        }
+
+
     }
 
 
     public void drawMaze() { // draws current maze
-        StdDraw.setCanvasSize(900,900);
-
         StdDraw.enableDoubleBuffering();
-        StdDraw.setPenColor(StdDraw.BLACK);
+        StdDraw.setCanvasSize(900, 900);
+        StdDraw.setXscale(-100, 100);
+        StdDraw.setYscale(-100, 100);
 
-        StdDraw.filledRectangle(1920/2,1080/2 + 100,300,300); // this will be the area that
+        StdDraw.setPenColor(StdDraw.BLACK);
+        StdDraw.filledRectangle(1920 / 2, 1080 / 2 + 100, 300, 300); // this will be the area that
+
+
+        double xCord = -70.00;
+        double yCord =  70.00;
+        for (int iRows = 1; iRows < maze.length; iRows++) { // loop through the tile maze and create tiles on Canvas
+
+            for(int iCol = 1; iCol < maze[0].length;iCol++) {
+                StdDraw.setPenColor(Color.RED);
+                StdDraw.filledSquare(xCord,yCord,tileLength/2);
+                StdDraw.setPenColor(Color.BLACK);
+                StdDraw.square(xCord,yCord,tileLength/2);
+            // create tile in here
+                xCord = xCord + tileLength;
+            }
+            // when tile is done
+                yCord = yCord + tileLength;
+
+        }
+
+
+
     }
 
     public static void main(String[] args) {
 
     }
-
-
-
-
 
 
 }
